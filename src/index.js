c6a84f2091ffc14d8cd746cf67f6225820aada67
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-23 02:59:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,104 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (property) => {
+    /*Object.defineProperty(Object.prototype, property, {
+        get: function() { return Object.prototype._value; },
+        set: function(newVal) {
+            Object.prototype._value = newVal
+        },
+        enumerable: true,
+    });*/
+    return property
+};
+
+const createNotEnumerableProperty = (property) => {
+    Object.defineProperty(Object.prototype, property, {
+        get: function(){ 
+            return Object.prototype._value; 
+        },
+        set: function(value){
+            Object.prototype._value = value
+        },
+    });
+    return property
+};
+
+const createProtoMagicObject = () => {
+    let protoMagicObject = new Function()
+    Object.defineProperty(protoMagicObject, '__proto__', { value: null })
+    protoMagicObject.prototype = protoMagicObject.__proto__
+    return protoMagicObject
+};
+
+let incCount = 0
+Function.prototype.valueOf = function() {
+    return incCount
+};
+const incrementor = () => {
+    incCount++
+    return incrementor
+};
+
+let asyncIncCount = 0
+const asyncIncrementor = () => {
+    return new Promise((resolve) => {
+        asyncIncCount++
+        return resolve(asyncIncCount)
+    })
+};
+
+const createIncrementer = () => {
+    return {
+        i: 0,
+        next() {
+            return { value: ++this.i }
+        },
+        [Symbol.iterator]() {
+            return {
+                next: () => {
+                    return this.next()
+                },
+            };
+        },
+    };
+}
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = (args) => {
+    return new Promise((resolve) => {
+        setTimeout(function(){
+            resolve(args)
+        }, 1100)
+    })
+};
+
+
+const getDeepPropertiesCount = (object) => {
+    let properties = Object.getOwnPropertyNames(object);
+    let answer = properties.length;
+    properties.forEach(property => {
+        if (Object.getOwnPropertyNames(object[property]).length > 0){
+            answer += getDeepPropertiesCount(object[property]);
+        }
+    })
+    return answer;
+};
+
+
+
+const createSerializedObject = function() {
+    return null
+};
+
+const sortByProto = (objects) => {
+    return objects.map((object, i) => {
+        let count = 0
+        while (object = object.__proto__){
+             count++
+        }
+        return [count, objects[i]]
+    }).sort((l, r) => l[0] - r[0]).map(([object, i]) => i)
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
